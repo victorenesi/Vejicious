65b0fa75f3a88e4804fbd4209dcc77c9dea0499d
Victor Enesi <victor.enesi@gmail.com>
2022-12-08 22:07:43 +0000

    Created Adaptor subfolder and 'CartListAdapter'. Created IntroActivity and 'activity_intro' layout.

---
+++ app/src/main/java/uk/ac/tees/b1196119/vejicious/Activity/IntroActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.b1196119.vejicious.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
