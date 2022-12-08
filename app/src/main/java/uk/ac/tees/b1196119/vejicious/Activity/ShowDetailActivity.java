9aea4be5f26bdc190858da80e6f663688f27a624
Victor Enesi <victor.enesi@gmail.com>
2022-12-08 23:28:40 +0000

    Created 'ShowDetailActivity' and 'FoodDomain' activities. Created Domain subfolder and 'CategoryDomain' and 'FoodDomain' classes.

---
+++ app/src/main/java/uk/ac/tees/b1196119/vejicious/Activity/ShowDetailActivity.java
@@ -0,0 +1,82 @@
+package uk.ac.tees.b1196119.vejicious.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import uk.ac.tees.b1196119.vejicious.R;
+
+public class ShowDetailActivity extends AppCompatActivity {
+    private TextView AddToCartBtn;
+    private TextView titleTxt, feeTxt, descriptionTxt, numberOrderTxt;
+    private ImageView plusBtn, minusBtn, picFood;
+    private FoodDomain object;
+    int numberOrder = 1;
+    private ManagementCart managementCart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_detail);
+
+        managementCart = new ManagementCart(this);
+        initView();
+        getBundle();
+    }
+
+    private void getBundle() {
+        object = (FoodDomain) getIntent().getSerializableExtra("object");
+
+        int drawableResourceId = this.getResources().getIdentifier(object.getPic(), "drawable", this.getPackageName());
+        Glide.with(this)
+                .load(drawableResourceId)
+                .into(picFood);
+
+        titleTxt.setText(object.getTitle());
+        feeTxt.setText("$" + object.getFee());
+        descriptionTxt.setText(object.getDescription());
+        numberOrderTxt.setText(String.valueOf(numberOrder));
+
+        plusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberOrder = numberOrder + 1;
+                numberOrderTxt.setText(String.valueOf(numberOrder));
+            }
+        });
+
+        minusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (numberOrder > 1) {
+                    numberOrder = numberOrder - 1;
+                }
+                numberOrderTxt.setText(String.valueOf(numberOrder));
+            }
+        });
+
+        AddToCartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                object.setNumberInCart(numberOrder);
+                managementCart.insertFood(object);
+            }
+        });
+
+    }
+
+    private void initView() {
+        AddToCartBtn = findViewById(R.id.AddToCartBtn);
+        titleTxt = findViewById(R.id.titleTxt);
+        feeTxt = findViewById(R.id.priceTxt);
+        descriptionTxt = findViewById(R.id.descriptionTxt);
+        numberOrderTxt = findViewById(R.id.numberOrderTxt);
+        plusBtn = findViewById(R.id.plusBtn);
+        minusBtn = findViewById(R.id.minusBtn);
+        picFood = findViewById(R.id.picfood);
+
+    }
+}
