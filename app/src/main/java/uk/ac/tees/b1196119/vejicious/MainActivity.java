3a14caa9d35192a7e9f75864bf42f284f7631e62
Victor Enesi <victor.enesi@gmail.com>
2022-12-08 13:55:08 +0000

    Initial commit by Victor Enesi (B1196119)

---
+++ app/src/main/java/uk/ac/tees/b1196119/vejicious/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.b1196119.vejicious;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
